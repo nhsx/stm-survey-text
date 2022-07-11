9002e699da7be0912e5a7227c8fa1ea6bfde4c05
Paul Carroll <97230649+pauliecarroll@users.noreply.github.com>
2022-07-11 16:19:11 +0100

    Update and rename preprocess_function.R to functions.R

--- R/main/functions.R
+++ R/main/functions.R
@@ -1,36 +1,21 @@
-#source("./R/main/libraries.R")
-source("~/Github/stm-survey-text-1/R/main/libraries.R")
+################################################################################
+## Title: preprocess_functions
+## Last Update: 11/07/2022
+## Version: 1.0
+## Developer Contact: analytics-unit@nhsx.nhs.uk
+################################################################################
 
-
+# function to calculate maximum, minimum and average number of words 
 text_length <- function(data) {
-  #' maximum, minimum and average number of words 
   word_max <- max(str_count(data, "\\w+"))
   word_min <- min(str_count(data, "\\w+"))
   word_mean <- mean(str_count(data, "\\w+"))
-
   print(paste("Word count - max:", word_max, "min:", word_min, "mean:", word_mean))
 }
 
-
-
+# function to removes NAs and corrects data type. 
 prep_dataframe <- function(df, filter_sent = FALSE){
-  #' removes NAs and corrects data type. 
   df <- na.omit(df) 
-  #df$criticality[df$criticality < -4] <- -4
-  #df$question <- str_remove_all(df$question, "[Trust ABCDQ-]")
-  
-  #df$code <- as.factor(df$code) # data type set as string
-  #df$label <- as.factor(df$label)
-  #df$subcategory <- as.factor(df$subcategory)
-  df$Source <- as.factor(df$Source)
-  df$Stakeholder <- as.factor(df$Stakeholder)
-  #df$feedback <- as.character(df$feedback)
-  df$Response <- as.character(df$Response)
-  #df$organization <- as.factor(df$organization)
-  df$assignment <- as.factor(df$assignment)
-  #df$question <- as.factor(df$question)
-  #df$criticality <- as.numeric(df$criticality)
-  df$sentiment <- as.numeric(df$sentiment)
   
   if (filter_sent == TRUE){
     df <- df[(df$sentiment > 0.05) | (df$sentiment < -0.05),] 
@@ -39,13 +24,6 @@ prep_dataframe <- function(df, filter_sent = FALSE){
   }
   return(df)
 }
-
-
-
-#stopwords
-high_freq <- c("staff", "helpful", "care", "nothing", "xxxx")
-mystopwords <- c(stopwords('en'), high_freq)
-
 
 lemmatiser <- function(data, lemma = TRUE){
   #' Standardises words: lemmatises words if lemma = True
@@ -57,8 +35,7 @@ lemmatiser <- function(data, lemma = TRUE){
   return(out)
 }
 
-
-clean_text <- function(data, StopWords = mystopwords, mintermfreq=2, lemma = TRUE, ngram = FALSE){
+clean_text <- function(data, StopWords, mintermfreq=2, lemma = TRUE, ngram = FALSE){
   #' Takes dataframe -> corpus with metadata.
   #' Cleans text: lowercase, expands contractions, removes digits and punctuation, 
   #' tokenises and remove stopwords.
@@ -70,12 +47,10 @@ clean_text <- function(data, StopWords = mystopwords, mintermfreq=2, lemma = TRU
   docvars(t1, "doc_id") <- data$row_index
   docvars(t1) <- data
   
-  
   t1 <- t1 %>% tolower()%>% textclean::replace_contraction()
   t2 <- gsub("[^[:alnum:][:space:]]","", t1) 
   t2 <- gsub("[0-9]+", " ", t2)
   t2 <- gsub(" *\\b[[:alnum:]]{1}\\b *", " ", t2)
-  
   
   if(lemma == TRUE){
   t3 <- lemmatiser(t2)
@@ -83,14 +58,12 @@ clean_text <- function(data, StopWords = mystopwords, mintermfreq=2, lemma = TRU
   t3 <- lemmatiser(t2,lemma=FALSE)
   }
 
-  
   if(ngram == TRUE){
     t4 <- quanteda::tokens(t3)
     t4 <- tokens_ngrams(t4, n = c(1,2), concatenator = " ")
   } else{
     t4 <- quanteda::tokens(t3)
   }
-  
   
   # t4 <- quanteda::tokens(t3)
   token <- tokens_remove(t4, pattern = StopWords)
@@ -101,8 +74,6 @@ clean_text <- function(data, StopWords = mystopwords, mintermfreq=2, lemma = TRU
   outputs <- list("Tokens" = token, "DocMatrix" = docfm)
   return(outputs)
 }
-
-
 
 convert_to_stm <- function(dtm, docva){
   #' takes column/list of text and creates document feature matrix (dfm)
@@ -129,14 +100,52 @@ sentAnalysis <- function(x, df){
   
   # removes row with NAs
   filteredsentiment <- na.omit(sentiment)
-  
   newdataset1 <-df[-which(is.na(sentiment$compound)),]
-  
   noRemoved <- length(sentiment$text) - length(filteredsentiment$text)
-  print(paste("The dataframe now contains", length(sentiment$text) ,"rows.", noRemoved, "rows have been removed."))
-  
+  print(paste("The dataframe now contains", length(sentiment$text) ,"rows.", 
+              noRemoved, "rows have been removed."))
   newdataset1["sentiment"] <- as.numeric(filteredsentiment$compound)
   
   return(newdataset1)
 }
 
+labeldf <- function(model, data, k){
+  # k - the number of topics in the model
+  stmdf <- stm::make.dt(model, meta=data)
+  stmdf$`Most Probable Topic` <- 
+    apply(stmdf[,2:k], 1, function(x){ which(x == sort(x, decreasing = TRUE)[1])})
+  stmdf$`Second Most Probable Topic` <- 
+    apply(stmdf[,2:k], 1,function(x){ which(x == sort(x, decreasing = TRUE)[2])})
+  return(stmdf)
+}
+
+readname <- function(prompttext) # prompt user for the text 
+{ 
+  n <- readline(prompt = prompttext)
+  return(n)
+}
+
+
+# prompt user for search terms 
+searchtext <- function(df, terms){
+  
+  for (i in c(1:length(terms))){
+    rows <- grep(terms[[i]], df$Response, ignore.case =TRUE)
+  }
+  
+  results <- df[rows,]
+  return(results)
+}
+
+# get similar words 
+extendTerms <- function (string){
+  extendterms <- list()
+  syn <- unlist(synonyms(string, "NOUN"))
+  
+  for (el in syn){
+    if(el %in% stmdata$vocab){
+      extendterms[[length(extendterms) + 1]] <- el}
+  }
+  
+  return(unlist(extendterms))
+}
