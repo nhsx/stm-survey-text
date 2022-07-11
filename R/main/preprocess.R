4e871123f300f6193d088d12e333bb12247402b1
Paul Carroll <97230649+pauliecarroll@users.noreply.github.com>
2022-07-11 16:20:34 +0100

    Update preprocess.R

--- R/main/preprocess.R
+++ R/main/preprocess.R
@@ -1,20 +1,16 @@
-### Data pre-processing ###
-#' This file load and prepares the data to be used by stm package. 
+################################################################################
+## Title: preprocess
+## Last Update: 11/07/2022
+## Version: 1.0
+## Developer Contact: analytics-unit@nhsx.nhs.uk
+################################################################################
 
-#source("./R/main/libraries.R")
-#source("./R/main/preprocess_function.R")
-
-source("~/Github/stm-survey-text-1/R/main/libraries.R")
-source("~/Github/stm-survey-text-1/R/main/preprocess_function.R")
-
-### Load Data into R ####
-#path <- "./data/text_data.csv"
-#path <- "~/Github/stm-survey-text-1/data/text_data.csv"
-path <- "/Users/paul.carroll/Desktop/DHCPCoded2.csv"
-data <- read_csv(path)
-
-# Summary statistics for the data  
+# ------------ Data pre-processing ------------------------
+sapply(data, class)
 summary(data)
+
+#stopwords
+mystopwords <- c(stopwords('en'), highFreq)
 
 # Idenitfy columns that contain missing values, then idenitfy the rows with
 # these missing values.
@@ -23,39 +19,27 @@ data[rowSums(is.na(data)) > 0, ]
 # Summary of text data length
 text_length(data$Response)
 
-
 # Split data training set and test set. The test set is retained to validate the
-# method at the end of development.
+# method at the end of development.  
 set.seed(1)
-sample <- sample(nrow(data), 500)
+sample <- sample(nrow(data), ceiling(nrow(data)/20))
 test_set <- data[sample, ]
 train_set <- data[-sample, ]
 
-
-### Sentiment Analysis ####
+# ------------ Sentiment Analysis ------------------------
 # Vader is used to perform sentiment analysis on the text and adds a column to
 # the dataframe with the sentiment scores. The can be run and saved using the
 # following code. This can take a long time, so the sentiment score has been
 # saved and can be read in using the following code (commented out).
-
 train_set <- sentAnalysis(train_set$Response, train_set)
 
-write.csv(train_set,"/Users/paul.carroll/Github/stm-survey-text-1/data/trainsetwithsentiment.csv")
-#write.csv(train_set, "./data/trainsetwithsentiment.csv")
-
-# Saved sentiment score. 
-# train_set <- read_csv("./data/trainsetwithsentiment.csv")
-# train_set <- train_set[,c(2:10)] 
-
-### Cleaning and pre-processing data ####
+# ------------ Cleaning and pre-processing data ------------------------
 
 #From the training set, remove rows with NAs values and explicitly specify the
 #data types of variables. This is dataset specific. Outliers of the
 #"criticality" variable are imputed to be between -4 and 4 The variables are
 #categorised as numeric, factors (for categorical data), or character strings.
-
 train_set <- prep_dataframe(train_set)
-
 
 # The feedback responses ("feedback") is cleaned and pre-processed to be used
 # for the topic modelling. A corpus of is generated and the metadata is
@@ -64,14 +48,11 @@ train_set <- prep_dataframe(train_set)
 # text is tokenised. The text_cleaned object produced contains
 # text_cleaned$Tokens which is the text tokenised and text_cleaned$DocMatrix,
 # which is the document- feature matrix.
-
-text_cleaned <- clean_text(train_set)
+text_cleaned <- clean_text(train_set,StopWords = mystopwords)
 
 # The document feature matrix is coverted to a format that is used by stm 
 # package.
-
 stmdata <- convert_to_stm(text_cleaned$DocMatrix, train_set)
-
 
 # Plot of the distribution of tokens per document in the pre-processed text. 
 w <- rowSums(text_cleaned$DocMatrix)
@@ -82,9 +63,6 @@ barplot(w,
 boxplot(w, xlab = "Number of Tokens", horizontal = TRUE, 
         main = "Boxplot of Number of Tokens per Document")
 
-
 # The data is processed and can be used with stm package. The model selection
 # is done using `modelSelection.R`.
-
-
 
